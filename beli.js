836d6e74d8b1e08bcd7efc7592019e5b183276c0
Aditya Respati <dityo.hario@gmail.com>
2018-12-12 17:15:54 +0700

    commit

--- beli.js
+++ beli.js
@@ -1,15 +1,20 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+function beli(uang, obj){
+  return new Promise(function(resolve, reject){
+
+    console.log(`Saya pergi membeli ${obj.item}`)
+
+    setTimeout(function(){
+      let kembalian = uang - obj.harga
+      if (kembalian > 0) {
+        console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+        resolve(kembalian)
+      }else{
+        // console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+        reject(`uang saya gak cukup nich sisa uang cuma ${uang}`)
+      }
+    }, obj.waktu);
+
+  })
 }
 
 module.exports = beli;
