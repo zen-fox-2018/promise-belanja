8d6916dedce499a96ba8d4a239dba498500a409f
ACER <fauziaazia171097@gmail.com>
2018-12-11 23:17:46 -0500

    done

--- beli.js
+++ beli.js
@@ -1,15 +1,17 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+function beli(uang, obj){
+  return new Promise ((resolve,reject) => {
+    console.log(`Saya pergi membeli ${obj.item}`)
+    setTimeout(function(){
+        let kembalian = uang - obj.harga
+        if (uang > obj.harga) {
+          console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+          resolve(kembalian)
+        }else{
+          console.log(`uang gk cukup nih buat beli ${obj.item} sisa uangnya cuma ${uang}`);
+          reject(uang)
+        }
+    }, obj.waktu);
+  })
 }
 
 module.exports = beli;
