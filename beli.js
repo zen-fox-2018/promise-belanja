a598154b27257d48ed9f330a279ce2e5c63676c5
Arief Rachman <arievrchman@gmail.com>
2018-12-12 10:58:59 +0700

    done

--- beli.js
+++ beli.js
@@ -1,15 +1,27 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+function beli(uang, obj) {
+
+  const belanja = new Promise((resolve, reject) => {
+
+    console.log(`Saya pergi membeli ${obj.item}`);
+
+    setTimeout(function () {
+
+      let kembalian = uang - obj.harga
+
+      if (kembalian > 0) {
+        console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+        resolve(kembalian);
+      } else {
+        console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
+        reject(0);
+      }
+
+    }, obj.waktu);
+
+  });
+
+  return belanja;
+
 }
 
 module.exports = beli;
