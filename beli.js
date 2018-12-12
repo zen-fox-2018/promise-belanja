13bb05aae7769715da4cb53256713e9e924ae09c
Zhang <zhang@ZhangMBP15.local>
2018-12-12 11:12:56 +0700

    done

--- beli.js
+++ beli.js
@@ -1,15 +1,18 @@
-function beli(uang, obj, cb){
+function beli(uang, obj){
   console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+
+  return new Promise((res, rej) => {
+    setTimeout(function(){
+      let kembalian = uang - obj.harga
+      if (kembalian > 0) {
+        console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+        res(kembalian)
+      }else{
+        console.log(`uang gk cukup nih buat beli ${obj.item} uangnya kurang ${Math.abs(kembalian)}`);
+        rej(uang)
+      }
+    }, obj.waktu);
+  })
 }
 
 module.exports = beli;
